3946bb22e0ea3de10288b1e7550d190982f1373c
Sinha-Ujjawal <https://github.com/Sinha-Ujjawal>
2022-06-05 01:24:09 +0530

    build files for github pages

---
+++ build.bat
@@ -0,0 +1,3 @@
+elm make --optimize --output=elm.js src/Main.elm
+
+uglifyjs elm.js --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" --mangle --output elm.min.js
